6431107017b722e1a8596cb944ee57872dee6036
So Jeong Lee <sj27419@gmail.com>
2023-05-19 00:10:13 +0900

    fix 피드백 반영한거 변경

--- accounts/models.py
+++ accounts/models.py
@@ -34,7 +34,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     # id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     id = models.CharField(max_length=64, primary_key=True, verbose_name="id", unique=True)
 
-    #username = models.CharField(max_length=17, verbose_name="아이디", unique=True)
+    username = models.CharField(max_length=17, verbose_name="아이디", unique=True) # 이거 없애니까 새로운 에러 나서 일단은 다시 부활시킴
     nickname = models.CharField(max_length=100, verbose_name="이름", null=True)
     date_joined = models.DateTimeField(auto_now_add=True, verbose_name='가입일', null=True, blank=True)
 
